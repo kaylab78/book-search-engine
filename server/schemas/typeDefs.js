de194a1a044caa8649c0c4f1a5b8b5c3ed14868a
kaylab78 <kaylabackus78@gmail.com>
2022-09-10 09:54:22 -0500

    Attempted to add typeDefs. Missing a few. Will discuss with tutor.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,37 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: 
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
     type Query {
-        helloWorld: String
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook()
+        removeBook(bookId: ID!): User
     }
 `;
 
