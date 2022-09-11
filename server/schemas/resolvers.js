8741c25fa59590a918f4c7bd1f3193cbc9e537c8
kaylab78 <kaylabackus78@gmail.com>
2022-09-11 09:46:32 -0500

    Wrote Query for resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,11 +4,21 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        me: async () => {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('books');
+                
+                return userData;
+            }
 
+            throw new AuthenticationError('Not logged in');
         },
-        user: async () => {
-
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .popluate('books');
         }
     },
 
@@ -23,7 +33,7 @@ const resolvers = {
 
         },
         removeBook: async () => {
-            
+
         }
     }
 };
