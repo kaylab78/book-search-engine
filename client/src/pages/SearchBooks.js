0e32ee53dec092c9e543f0b13a5c650aba9ecea2
kaylab78 <kaylabackus78@gmail.com>
2022-09-11 19:54:16 -0500

    Edited SavedBook and SearchBooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -67,11 +67,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook({ variables: { bookData: { ...bookToSave } } });
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      await saveBook({ variables: { bookData: { ...bookToSave } } });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
